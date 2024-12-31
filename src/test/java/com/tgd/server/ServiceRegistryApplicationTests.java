1cc713d5b0744800337ec01fb446c30f0a0db7bf
rakesh <rakesh@rakesh>
2024-12-31 09:56:25 +0530

    initial commit

---
+++ src/test/java/com/tgd/server/ServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tgd.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
